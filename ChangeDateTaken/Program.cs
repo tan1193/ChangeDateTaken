376cff303a00ccf92eb7819471b26a19798a9693
Tan Hoang <hoangtan1193@gmail.com>
2023-07-19 16:20:43 +0700

    update readme

--- ChangeDateTaken/Program.cs
+++ ChangeDateTaken/Program.cs
@@ -1,5 +1,4 @@
 ﻿using ExifLibrary;
-using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 Console.WriteLine("Enter your path: ");
@@ -11,15 +10,12 @@ var files = System.IO.Directory.GetFiles(@$"{name}", "*.jpg");
 foreach (var item in files)
 {
     var jsonFile = System.IO.File.ReadAllText(@$"{item}.json");
-    if (jsonFile is null)
-    {
-        continue;
-    }
+
     // step 3: write date taken to image
     JObject jsonObject = JObject.Parse(jsonFile);
     long photoTakenTimestamp = (long)jsonObject["photoTakenTime"]["timestamp"];
     DateTime photoTakenDateTime = DateTimeOffset.FromUnixTimeSeconds(photoTakenTimestamp).UtcDateTime;
-  
+
     var file = ImageFile.FromFile(item);
     file.Properties.Set(ExifTag.DateTime, photoTakenDateTime);
     file.Properties.Set(ExifTag.DateTimeDigitized, photoTakenDateTime);
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ChangeDateTaken
+
+This repo reads all the .jpg files in a given folder, reads the JSON file with the same name as the image, extracts the photo taken time from the JSON file, and writes the photo taken time to the image's EXIF metadata. 
